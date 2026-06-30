28c225af9449a5e2dd8f45892572b09973b55f58
Jarcom <m.fajarmuhaimin_ti24@nusaputra.ac.id>
2026-07-01 01:18:12 +0700

    Delete Tugas1.java

--- Tugas1.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class Tugas1 {
-    public static void main(String[] args) {
-        String belajar = JOptionPane.showInputDialog("Anda sedang belajar apa?");
-        JOptionPane.showMessageDialog(null,
-                "Belajar " + belajar + " sangat mudah");
-    }
-}
